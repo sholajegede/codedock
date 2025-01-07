6ab2334e2d8dc674bf4367fd6aa8d13024474180
Shola Jegede <jegedeshola@gmail.com>
2025-01-07 19:11:26 +0100

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://clean-dory-22.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
